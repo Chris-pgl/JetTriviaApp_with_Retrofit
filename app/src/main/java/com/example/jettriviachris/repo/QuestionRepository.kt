f2efdeffa3434cb8680d6f1341692bf3ebf3a56d
Chris <cr1s@hotmail.it>
2026-05-14 16:25:38 +0200

    finish repository with dataOrException

---
+++ app/src/main/java/com/example/jettriviachris/repo/QuestionRepository.kt
@@ -0,0 +1,34 @@
+package com.example.jettriviachris.repo
+
+import android.util.Log
+import com.example.jettriviachris.data.DataOrException
+import com.example.jettriviachris.model.QuestionItem
+import com.example.jettriviachris.network.QuestionApi
+import javax.inject.Inject
+
+
+class QuestionRepository @Inject constructor(private val api: QuestionApi){
+
+    private val dataOrException =
+        DataOrException<ArrayList<QuestionItem>,
+                Boolean,
+                Exception>()
+
+
+    suspend fun getAllQuestions(): DataOrException<ArrayList<QuestionItem>, Boolean, Exception>{
+        try {
+
+            dataOrException.loading = true
+            dataOrException.data = api.getAllQuestions()
+            if(dataOrException.data.toString().isNotEmpty()) dataOrException.loading = false
+            
+        }catch (exception: Exception){
+            dataOrException.e = exception
+            Log.d("Exc", "getAllQuestions: ${dataOrException.e!!.localizedMessage}")
+        }
+
+        return dataOrException
+    }
+
+
+}
