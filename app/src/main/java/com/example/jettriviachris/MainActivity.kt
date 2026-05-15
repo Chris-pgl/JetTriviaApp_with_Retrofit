d868946f88f2f12249eba32815060a990517dea4
Chris <cr1s@hotmail.it>
2026-05-15 15:31:37 +0200

    add viewModel for question

--- app/src/main/java/com/example/jettriviachris/MainActivity.kt
+++ app/src/main/java/com/example/jettriviachris/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.jettriviachris
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,6 +11,8 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.jettriviachris.screens.QuestionViewModel
 import com.example.jettriviachris.ui.theme.JetTriviaChrisTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,9 +28,28 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+@Composable
+fun MyApp(modifier: Modifier){
+
+}
 
 @Composable
-fun MyApp(modifier: Modifier) {
+fun TriviaHome( viewModel: QuestionViewModel = hiltViewModel()) {
+    Questions(viewModel)
+}
+
+@Composable
+fun Questions(viewModel: QuestionViewModel) {
+    val questions = viewModel.data.value.data?.toMutableList()
+    if (viewModel.data.value.loading == true){
+        Log.d("Loading", "Questions: ...Loading... ")
+    }else{
+        Log.d("Loading", "Questions: Loading STOPPED..")
+        questions?.forEach { questionItem ->
+            Log.d("Result", "Questions: ${questionItem.question}")
+        }
+    }
+
 
 }
 
